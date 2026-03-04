f7b6b693be9254613bfd34afb296fb0f5d62eccb
anusanth26 <anusanth2645@gmail.com>
2026-03-04 23:56:51 +0530

    feat: Implement backend services and API routes for canvas and folder management, including a FolderView component.

--- canvas/apps/http-backend/src/routes/canvas.ts
+++ canvas/apps/http-backend/src/routes/canvas.ts
@@ -5,13 +5,16 @@ import {
 	deleteCanvas,
 	getCanvas,
 	getCanvases,
+	searchCanvases,
 	updateCanvas,
 } from "../controller/canvas";
 import { authMiddleware } from "../middleware/auth";
 export const canvasRouter: RouterType = Router();
 
 canvasRouter.get("/", authMiddleware, getCanvases);
+canvasRouter.get("/search", authMiddleware, searchCanvases);
 canvasRouter.get("/:roomId", authMiddleware, getCanvas);
 canvasRouter.post("/create-canvas", authMiddleware, createCanvas);
 canvasRouter.put("/:roomId", authMiddleware, updateCanvas);
 canvasRouter.delete("/:roomId", authMiddleware, deleteCanvas);
+
